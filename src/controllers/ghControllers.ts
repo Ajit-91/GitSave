4615ec06cd8a007e21c5b11e1a2e610c4d766851
Ajit-91 <ajitkumar12105@gmail.com>
2023-05-20 18:54:14 +0530

    improved table format

--- src/controllers/ghControllers.ts
+++ src/controllers/ghControllers.ts
@@ -9,70 +9,69 @@ import Repo from "../interfaces/Repo";
 export const addRepoData: RequestHandler = catchError(async (req, res, next) => {
 
     const url: string = req.body.url
+    
     // validate url
     const regex : RegExp = /^https:\/\/api\.github\.com\/users\/[^/]+\/repos$/;
-    if(!regex.test(url)) throw new AppError(400, "Please provide a valid github url")
+    if(!regex.test(url)) throw new AppError(400, "Please provide a valid github url to fetch data")
     
-    // fetcj data from given url using axios
+    // fetch data from given url using axios
     const response: AxiosResponse = await axios.get(url)
-    console.log({ response })
+    // console.log({ response })
     if(response.status !== 200 || !response.data) throw new AppError(400, "Cannot fetch data from the given url")
 
      // save data to postgresql database
-    const promises = response.data.map((repo : Repo) => {
+    const promises = response.data.map((repo : any) => {
+        const owner =   JSON.stringify({
+            id : repo.owner.id,
+            avatar_url : repo.owner.avatar_url,
+            html_url : repo.owner.html_url,
+            type : repo.owner.type,
+            site_admin : repo.owner.site_admin
+        })
+
         return pool.query(`
-        INSERT INTO repos (
-        id, 
-        name, 
-        html_url, 
-        description, 
-        created_at, 
-        open_issues, 
-        watchers, 
-        owner_id, 
-        owner_avatar_url, 
-        owner_html_url, 
-        owner_type, 
-        owner_site_admin
-        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10 , $11, $12)`, 
-        [repo.id, repo.name, repo.html_url, repo.description, repo.created_at, repo.open_issues, repo.watchers, repo.owner.id, repo.owner.avatar_url, repo.owner.html_url, repo.owner.type, repo.owner.site_admin])
+            INSERT INTO repositories (
+            id, 
+            name, 
+            html_url, 
+            description, 
+            created_at, 
+            open_issues, 
+            watchers, 
+            owner
+            ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`, 
+        [repo.id, repo.name, repo.html_url, repo.description, repo.created_at, repo.open_issues, repo.watchers, owner])
     })
-    const data = await Promise.all(promises)
-    console.log({ data })
+
+    await Promise.all(promises)
 
     // send response
     res.status(200).json({
-        status: "success"
+        success: true,
     })
 })
 
 
 export const getRepoData: RequestHandler = catchError(async (req, res, next) => {
+    const id : string = req.params.id
+    // check if id is valid integer ort not, if not then throw error
+    if(!Number(id)) throw new AppError(400, "Please provide a valid id")
+
     // fetch data from postgresql database
-    const data = await pool.query(`SELECT * FROM repos WHERE id = $1`, [req.params.id])
-    
-    if(data.rows.length === 0) throw new AppError(404, "No data found")
-    
-    const finalData : Repo = {
-        id: data.rows[0].id,
-        name: data.rows[0].name,
-        html_url: data.rows[0].html_url,
-        description: data.rows[0].description,
-        created_at: data.rows[0].created_at,
-        open_issues: data.rows[0].open_issues,
-        watchers: data.rows[0].watchers,
-        owner: {
-            id: data.rows[0].owner_id,
-            avatar_url: data.rows[0].owner_avatar_url,
-            html_url: data.rows[0].owner_html_url,
-            type: data.rows[0].owner_type,
-            site_admin: data.rows[0].owner_site_admin
-        }
+    const repoData = await pool.query(`SELECT * FROM repositories WHERE id = $1`, [id])
+
+    let data : Repo | {}
+
+    if(repoData.rows.length === 0){
+        data = {}
+    }else{
+        data = repoData.rows[0]
     }
+    
     // send response
     res.status(200).json({
         success: true,
-        data: finalData
+        data
     })
 })
 
--- src/database.sql
+++ src/database.sql
@@ -1,15 +1,10 @@
-
-CREATE TABLE IF NOT EXISTS repos (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  html_url VARCHAR(255) NOT NULL,
-  description VARCHAR(255),
-  created_at VARCHAR(255) NOT NULL,
-  open_issues INTEGER NOT NULL,
-  watchers INTEGER NOT NULL,
-  owner_id VARCHAR(255) NOT NULL,
-  owner_avatar_url VARCHAR(255) NOT NULL,
-  owner_html_url VARCHAR(255) NOT NULL,
-  owner_type VARCHAR(255) NOT NULL,
-  owner_site_admin BOOLEAN NOT NULL
-);
+ CREATE TABLE IF NOT EXISTS repositories (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    html_url VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    created_at VARCHAR(255) NOT NULL,
+    open_issues INTEGER NOT NULL,
+    watchers INTEGER NOT NULL,
+    owner JSONB NOT NULL
+  );
